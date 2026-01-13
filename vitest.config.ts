7db35918dea30f015d56320f61c0d707ec784e5d
passive-income <mimiwiv@gmail.com>
2026-01-13 21:55:28 +0100

    adjusted typescript rule

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  // @ts-expect-error - vite version mismatch (local only - CI resolves correctly)
   plugins: [react()],
   test: {
     environment: 'jsdom',
